fb88f27efe35a59ee02ee1561b07edc83e910931
giusscos <giu.cosenza08@gmail.com>
2024-09-15 01:08:03 +0200

    Animate jumbo

--- components/HomeJumbo.vue
+++ components/HomeJumbo.vue
@@ -1,12 +1,36 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import gsap from 'gsap';
+
+onMounted(() => {
+  gsap.from('.title', {
+    duration: 3,
+    ease: "expo.inOut",
+    autoAlpha: 0,
+    y: '+=200%',
+    stagger: {
+      each: 0.1,
+    },
+  })
+})
+</script>
 
 <template>
   <section id="home">
-    <h2 class="title-big">
-      Frontend Developer <br> <b>freelancer</b>
+    <h2 class="title-big leading-none">
+      <!-- Frontend Developer <br> <b>Freelancer</b> -->
+      <span class="inline-block overflow-hidden">
+        <div class="title">Frontend</div>
+      </span> <span class="inline-block overflow-hidden">
+        <div class="title">Developer</div>
+      </span>
+      <span class="inline-block overflow-hidden">
+        <div class="title"><b>Freelancer</b></div>
+      </span>
     </h2>
-    <p class="paragraph-small leading-none w-2/3 lg:w-3/5">
-      Create websites with a focus on stunning animations, details, and user interactions.
+    <p class="paragraph-small leading-none w-2/3 lg:w-3/5 overflow-hidden">
+      <span class="inline-block title">
+        Create websites with a focus on stunning animations, details, and user interactions.
+      </span>
     </p>
   </section>
 </template>
