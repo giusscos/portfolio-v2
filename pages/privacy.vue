6010c0de71dabbbe890fe58805402ecbbbfbe026
giusscos <giu.cosenza08@gmail.com>
2024-10-20 15:58:05 +0200

    Add privacy-policy page

---
+++ pages/privacy.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <section>
+        <h1 class="title-big uppercase mb-10">
+            Privacy policy
+        </h1>
+
+        <h2 class="title-small text-error mb-2">
+            General information
+        </h2>
+        <p class="paragraph-small mb-6">
+            Giusscos' apps are all the Apps released in the AppStore under Giuseppe Pio Cosenza personal account.
+
+            On the most basic level, I value your privacy and make decisions in our development to respect it to the
+            best of our ability.
+        </p>
+
+        <h2 class="title-small text-error mb-2">
+            Storage and Sync
+        </h2>
+        <p class="paragraph-small mb-6">
+            <!-- All my Apps stores your data locally on the devices you use them, and syncs across devices using Apple's
+            iCloud. Specifically, the CloudKit portion of iCloud, storing data in your private iCloud database. -->
+            All my Apps stores your data locally on the devices you use them
+        </p>
+
+        <h2 class="title-small text-error mb-2">
+            Protection of Certain Personally-Identifying Information
+        </h2>
+        <p class="paragraph-small mb-6">
+            No Personally-Identifying Information is transmitted to external locations when using Giusscos's apps.
+
+            Non-personally identifiable information such as device type and operating system version to our servers.
+            This is used in aggregate to help facilitate development decisions.
+        </p>
+
+        <h2 class="title-small text-error mb-2">
+            Questions about data handling
+        </h2>
+        <p class="paragraph-small mb-6">
+            If you have a question about this privacy and how your data are collected, used and stored when you use
+            giusscos'apps, please feel free to email me at giusscos@icloud.com with the subject line “Data
+            handling”.
+        </p>
+    </section>
+</template>
